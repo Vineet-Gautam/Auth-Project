54bcb5bb81bd2bfda1e693207304aa5c3ccca6b3
Vineet Gautam <vineetgautamm@gmail.com>
2024-06-16 19:17:27 +0530

    custom error handler and a middleware handle error

--- api/index.js
+++ api/index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoute from "./routes/user.route.js"
-import authRoute from "./routes/auth.route.js"
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(
-    () => {
-        console.log("Connected to MongoDB")
-    }
-).catch((err) => {
-    console.log(err)
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.use(express.json());
@@ -26,5 +27,16 @@ app.listen(3000, () => {
 //   });
 // });
 
-app.use("/api/user", userRoute)
-app.use("/api/auth", authRoute)
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode
+  })
+});
